7cdc213ed3fce73fb4864ee75dd96d916d3a46b3
Pierre Baillet <oct@zoy.org>
2014-06-02 15:48:26 +0200

    WIP skeleton application

--- src/main.go
+++ src/main.go
@@ -4,35 +4,48 @@ import "code.google.com/p/flickgo"
 import "net/http"
 import "fmt"
 import "flag"
+import "os"
+import "log"
+
+var (
+	apiKey string
+	secret string
+)
+
+// Define and parse flags
+func init() {
+	flag.StringVar(&apiKey, "api_key", "", "Api Key")
+	flag.StringVar(&secret, "secret", "", "Secret Key")
+}
 
 func main() {
-  var err error
-  var sresponse * flickgo.SearchResponse
+	var err error
+	var sresponse *flickgo.SearchResponse
 
-  // Get the API keys from https://www.flickr.com/services/apps/by/oct
-  var api_key string
-  var secret  string
+	flag.Parse()
 
+	if apiKey == "" || secret == "" {
+		fmt.Println("Missing one or more command line options.")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
 
-  flag.StringVar(&api_key, "api_key", "1234", "Api Key")
-  flag.StringVar(&secret, "secret", "12", "Secret Key")
-  flag.Parse()
+	client := flickgo.New(apiKey, secret, http.DefaultClient)
 
-  fmt.Printf("%s %s", api_key, secret)
+	sparams := make(map[string]string)
+	sparams["text"] = "meditation"
+	sparams["sort"] = "interestingness-desc"
+	sparams["licence"] = "4,7"
 
+	sresponse, err = client.Search(sparams)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", sresponse.Photos[0].URL("n"))
 
-  client := flickgo.New( api_key, secret, http.DefaultClient )
+	log.Println("Starting Server")
+	http.Handle("/", http.FileServer(http.Dir("./public/")))
 
-
-  sparams := make(map[string]string)
-  sparams["text"] = "meditation"
-  sparams["sort"] = "interestingness-desc"
-  sparams["licence"] = "4,7"
-
-
-  sresponse, err = client.Search(sparams)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("%s", sresponse.Photos[0].URL("n"))
+	log.Println("Listening on 8080")
+	http.ListenAndServe(":8080", nil)
 }
