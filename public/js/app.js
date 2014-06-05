68d2b19028e7398e7bbb3718452e8b8333036d05
Pierre Baillet <oct@zoy.org>
2014-06-05 18:26:12 +0200

    Now we can load a subpage correctly

--- public/js/app.js
+++ public/js/app.js
@@ -16,22 +16,47 @@
     large: attr
   });
 
+  App.Tag = DS.Model.extend({
+    text: attr
+  });
+
+  App.PhotoInfo = DS.Model.extend({
+    title: attr,
+    description : attr,
+    tags: DS.hasMany('tag')
+  })
+
   App.Router.map(function() {
+    this.route('index', { path: '/' });
     this.resource('index', {path: '/search/:query'})
     this.resource('photo', {path: '/photo/:photo_id'})
 
-  })
+  });
+
+  // App.Router.reopen({
+  //   location: 'history'
+  // });
 
   App.PhotoRoute = Ember.Route.extend({
     model: function(params) {
-      return {photo: this.store.find('photo', params.photo_id)};
+      return {
+        photo: this.store.find('photo', params.photo_id),
+        info: this.store.find('photo_info', params.photo_id)
+      };
     }
   });
 
-
   App.IndexRoute = Ember.Route.extend({
+    beforeModel: function(transition) {
+      if (!transition.params.index.query) {
+        this.transitionTo('/search/meditation');
+      }
+    },
+
     model: function(params) {
-      return {query: params.query, result: this.store.find('photo',{ query: params.query })};
+      var sParam = params.query;
+
+      return {query: sParam, result: this.store.find('photo',{ query: sParam })};
     },
     actions: {
       search: function() {
--- src/main.go
+++ src/main.go
@@ -1,150 +1,169 @@
 package main
 
 import (
-	"code.google.com/p/flickgo"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"github.com/gorilla/mux"
-	"log"
-	"math"
-	"net/http"
-	"os"
-	"strconv"
+  "encoding/json"
+  "flag"
+  "fmt"
+  "github.com/gorilla/mux"
+  "github.com/octplane/flickgo"
+  "log"
+  "math"
+  "net/http"
+  "os"
+  "strconv"
 )
 
 var (
-	apiKey string
-	secret string
-	client *flickgo.Client
+  apiKey string
+  secret string
+  client *flickgo.Client
 )
-
-func infoURL(c *Client, args map[string]string) string {
-	argsCopy := clone(args)
-	argsCopy["extras"] += ",url_t"
-	return makeURL(c, "flickr.photos.getinfo", argsCopy, true)
-}
-
-// Searches for photos.  args contains search parameters as described in
-// http://www.flickr.com/services/api/flickr.photos.search.html.
-func (c *Client) Info(photoId string) (*SearchResponse, error) {
-	r := struct {
-		Stat   string         `xml:"stat,attr"`
-		Err    flickrError    `xml:"err"`
-		Photos SearchResponse `xml:"photos"`
-	}{}
-	if err := flickrGet(c, searchURL(c, args), &r); err != nil {
-		return nil, err
-	}
-	if r.Stat != "ok" {
-		return nil, r.Err.Err()
-	}
-
-	for i, ph := range r.Photos.Photos {
-		h, hErr := strconv.ParseFloat(ph.Height_T, 64)
-		w, wErr := strconv.ParseFloat(ph.Width_T, 64)
-		if hErr == nil && wErr == nil {
-			// ph is apparently just a copy of r.Photos.Photos[i], so we are
-			// updating the original.
-			r.Photos.Photos[i].Ratio = w / h
-		}
-	}
-	return &r.Photos, nil
-}
 
 // Define and parse flags
 func init() {
-	// https://www.flickr.com/services/apps/by/oct
-	flag.StringVar(&apiKey, "api_key", "", "Api Key")
-	flag.StringVar(&secret, "secret", "", "Secret Key")
+  // https://www.flickr.com/services/apps/by/oct
+  flag.StringVar(&apiKey, "api_key", "", "Api Key")
+  flag.StringVar(&secret, "secret", "", "Secret Key")
 }
 
 type Photo struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
-	Thumb string `json:"thumb"`
-	Large string `json:"large"`
+  Id    string `json:"id"`
+  Title string `json:"title"`
+  Thumb string `json:"thumb"` // n
+  Large string `json:"large"` // z
+}
+
+type PhotoInfo struct {
+  ID          string        `json:"id"`
+  Rotation    string        `json:"rotation,attr"`
+  License     string        `json:"license,attr"`
+  Title       string        `json:"title"`
+  Description string        `json:"description"`
+  Dates       flickgo.Dates `xml:"dates"`
+  Tags        []flickgo.Tag `xml:"tags>tag"`
+  Urls        []flickgo.Url `xml:"urls>url"`
+  Thumb       string        `json:"thumb"`
+  Large       string        `json:"large"`
+}
+
+func photoHandler(w http.ResponseWriter, r *http.Request) {
+  client := flickgo.New(apiKey, secret, http.DefaultClient)
+  photo_id := mux.Vars(r)["photo_id"]
+
+  finfo, err := client.GetInfo(photo_id)
+  if err != nil {
+    panic(err)
+  }
+
+  info := PhotoInfo{
+    finfo.ID,
+    finfo.Rotation,
+    finfo.License,
+    finfo.Title,
+    finfo.Description,
+    finfo.Dates,
+    finfo.Tags,
+    finfo.Urls,
+    finfo.Photo.URL("n"),
+    finfo.Photo.URL("z"),
+  }
+
+  // Serialize the modified kitten to JSON
+  j, err := json.Marshal(map[string]*PhotoInfo{"photoInfo": &info})
+  if err != nil {
+    panic(err)
+  }
+
+  w.Write(j)
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-	client = flickgo.New(apiKey, secret, http.DefaultClient)
+  photo_id := mux.Vars(r)["photo_id"]
+  query := r.FormValue("query")
+  fmt.Printf("Searching for %s\n", query)
 
-}
+  client = flickgo.New(apiKey, secret, http.DefaultClient)
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.FormValue("query")
+  max := 50
+  photos := make([]Photo, 0, max)
 
-	w.Header().Set("Content-Type", "application/json")
+  if photo_id != "" {
+    finfo, err := client.GetInfo(photo_id)
+    if err != nil {
+      panic(err)
+    }
 
-	client = flickgo.New(apiKey, secret, http.DefaultClient)
+    info := Photo{
+      Id:    finfo.ID,
+      Title: finfo.Title,
+      Thumb: finfo.Photo.URL("n"),
+      Large: finfo.Photo.URL("z"),
+    }
+    photos = append(photos, info)
 
-	fmt.Printf("Searching for %s\n", query)
+  } else {
+    sparams := make(map[string]string)
+    sparams["text"] = query
+    sparams["sort"] = "relevance" //interestingness-desc"
+    sparams["licence"] = "4,7"
+    //	sparams["others"] = "url_n,tags"
 
-	sparams := make(map[string]string)
-	sparams["text"] = query
-	sparams["sort"] = "relevance" //interestingness-desc"
-	sparams["licence"] = "4,7"
-	//	sparams["others"] = "url_n,tags"
+    var err error
+    var sresponse *flickgo.SearchResponse
 
-	var err error
-	var sresponse *flickgo.SearchResponse
+    sresponse, err = client.Search(sparams)
+    if err != nil {
+      panic(err)
+    }
 
-	sresponse, err = client.Search(sparams)
-	if err != nil {
-		panic(err)
-	}
+    var resCount int
+    var tempPhoto Photo
 
-	var resCount int
-	var tempPhoto Photo
+    resCount, err = strconv.Atoi(sresponse.Total)
+    if err != nil {
+      panic(err)
+    }
 
-	resCount, err = strconv.Atoi(sresponse.Total)
-	if err != nil {
-		panic(err)
-	}
+    for i := 0; i < int(math.Min(float64(resCount), float64(max))); i++ {
 
-	max := 50
+      tempPhoto = Photo{Id: sresponse.Photos[i].ID,
+        Thumb: sresponse.Photos[i].URL("n"),
+        Large: sresponse.Photos[i].URL("z"),
+        Title: sresponse.Photos[i].Title,
+      }
+      photos = append(photos, tempPhoto)
+    }
+  }
 
-	photos := make([]Photo, 0, max)
-
-	for i := 0; i < int(math.Min(float64(resCount), float64(max))); i++ {
-
-		tempPhoto = Photo{}
-		tempPhoto.Id = sresponse.Photos[i].ID + "@flickr"
-		tempPhoto.Thumb = sresponse.Photos[i].URL("n")
-		tempPhoto.Large = sresponse.Photos[i].URL("z")
-		tempPhoto.Title = sresponse.Photos[i].Title
-		photos = append(photos, tempPhoto)
-	}
-
-	// Serialize the modified kitten to JSON
-	j, err := json.Marshal(map[string][]Photo{"photos": photos})
-	if err != nil {
-		panic(err)
-	}
-
-	w.Write(j)
+  // Serialize the modified kitten to JSON
+  j, err := json.Marshal(map[string][]Photo{"photos": photos})
+  if err != nil {
+    panic(err)
+  }
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(j)
 }
 
 func main() {
 
-	flag.Parse()
+  flag.Parse()
 
-	if apiKey == "" || secret == "" {
-		fmt.Println("Missing one or more command line options.")
-		flag.PrintDefaults()
-		os.Exit(2)
-	}
+  if apiKey == "" || secret == "" {
+    fmt.Println("Missing one or more command line options.")
+    flag.PrintDefaults()
+    os.Exit(2)
+  }
 
-	log.Println("Starting Server")
+  log.Println("Starting Server")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/photos", searchHandler).Methods("GET")
-	r.HandleFunc("/api/v1/photos/:id", photoHandler).Methods("GET")
-	// /api/v1/photos/2251667479@flickr
-	http.Handle("/api/", r)
+  r := mux.NewRouter()
+  r.HandleFunc("/api/v1/photos/{photo_id:[a-z0-9]+}", searchHandler).Methods("GET")
+  r.HandleFunc("/api/v1/photoInfos/{photo_id:[a-z0-9]+}", photoHandler).Methods("GET")
+  r.HandleFunc("/api/v1/photos", searchHandler).Methods("GET")
 
-	http.Handle("/", http.FileServer(http.Dir("./public/")))
+  http.Handle("/api/", r)
+  http.Handle("/", http.FileServer(http.Dir("./public/")))
 
-	log.Println("Listening on 8080")
-	http.ListenAndServe(":8080", nil)
+  log.Println("Listening on 8080")
+  http.ListenAndServe(":8080", nil)
 }
